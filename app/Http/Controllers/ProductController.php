dab9601eef76dc0a623a4679fd5e55bbefb0fb39
Blast06 <lacalculadora06@hotmail.com>
2018-09-04 21:18:26 -0400

    product page added, fixed some bugs, and views refactoring

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,6 +12,19 @@ class ProductController extends Controller
     public function show($id)
     {
         $product = Product::find($id);
-        return "Mostrar datos del producto con id: $id $product";
+        $images = $product->images;
+
+        $imagesLeft = collect();
+        $imagesRight = collect();
+        foreach ($images as $key => $image) {
+            if($key % 2 == 0)
+                $imagesLeft->push($image);
+            else
+                $imagesRight->push($image);
+            
+        }
+        return view('products.show')->with(compact('product','imagesLeft','imagesRight'));
+
+        
     }
 }
