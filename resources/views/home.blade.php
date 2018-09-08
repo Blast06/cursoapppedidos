77670ce8dcb234f9ff3de4e30d205b236153232d
Blast06 <lacalculadora06@hotmail.com>
2018-09-08 17:47:59 -0400

    cart operations completed.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,9 +12,9 @@
             <div class="section ">
                 <h2 class="title text-center">Dashboard</h2>
 
-                @if (session('status'))
+                @if (session('notification'))
                     <div class="alert alert-success">
-                        {{ session('status') }}
+                        {{ session('notification') }}
                     </div>
                 @endif
                 <ul class="nav nav-pills nav-pills-primary" role="tablist">
